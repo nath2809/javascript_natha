fe5ba46831aea6a70e3c5818b4ddf296ee52c435
Nathalia Moncada <114375037+nath2809@users.noreply.github.com>
2023-11-20 08:48:53 -0500

    Add files via upload

---
+++ javascript/1/codigo.js
@@ -0,0 +1,61 @@
+
+// variable le agrego un dato, la variable es un espacio que guardar algun dato.
+//recipiente="papel";
+//alert(recipiente)
+
+//Declarar variable
+//var numero=25; //alcance global
+//let tiene menos alcance
+//let numero=30;+
+
+//SCOPE alcance de las variables
+//const nombre = "Natha"
+//alert( nombre)
+
+//Hoisting  fases de creacion y ejecucion
+
+//alert(numero);
+//let numero=3;
+
+//funcion nos devuelve un valor y se almacena en la variable nombre
+//let nombre =prompt("decime tu nombre");
+//alert( "hola" + nombre) // se muestra lo que se almacea en la variables
+
+
+// operador de asignación
+
+//let numero=10;
+//numero +=5
+
+//document.write(numero)
+
+//let numero= 47.5;
+//numero %=6
+//document.write(numero)
+
+
+//operadorea aritméticos
+
+//numero1=10;
+//numero2=5;
+//resultado=numero1 / numero2;
+
+// resultado = numero1**numero2;
+//alert(resultado)
+
+//numero1=10;
+//numero2=5;
+//resultado ++;
+//resultado= numero1;
+//alert(resultado)
+
+
+
+
+
+
+
+
+
+
+
---
+++ javascript/2/codigo2.js
@@ -0,0 +1,116 @@
+//dos cadena, concatenacion.
+
+//saludo="hola ";
+//pregunta="¿Como estas?";
+
+//concatenar
+//frase= saludo + pregunta;
+
+// resultado es 853 
+//numero1="53";
+//numero2="8";
+//frase= numero2 +numero1
+//document.write(frase)
+
+
+
+//nombre="lucas dalto";
+//frase='mi nombre es "lucas" y soy crack';
+//document.write(frase)
+
+
+//operadores
+
+
+//let numero=23;
+//let numero2=13;
+//let text="texto1";
+//let text2="texto2";
+
+//document.write(text===text2)
+
+//let numero=23;
+//let numero2=13;
+//document.write(numero>numero2)//
+
+
+//let numero=23;
+//let numero2=23;
+
+//let resultado= numero < numero2;
+//document.write(resultado)
+
+//let numero=23;
+//let numero2=5;
+
+//let resultado= numero > numero2;
+//document.write(resultado)
+
+
+
+
+
+// operadores lógicos, and, or ,not
+
+//let valor= true;
+//let valoor2=true;
+
+//let resultado=true;
+//document.write(resultado)
+
+//let valor=true;
+//let valor2=true;
+
+//let resultado = valor && valor2;//
+//let resultado2= valor|| valor2;
+//let resultado3= !valor;
+
+
+
+//num1=12;
+//num2=24;
+//afirmacion1= num1<num2;
+//afirmacion2= num1 !=num2;
+
+//document.write(afirmacion1 || afirmacion2)
+
+
+//parseInt convetir
+
+//holaComoEstas // camel case
+
+//document.getElementById()//metodo real
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
